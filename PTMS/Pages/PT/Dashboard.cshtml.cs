2ec0cecce9f69c99b254cd893fe09b7d6df6c65f
Timothy <Timothy@DESKTOP-6I1RKCJ>
2026-06-10 16:51:31 +0100

    PT dashboard 1st iteration

--- PTMS/Pages/PT/Dashboard.cshtml.cs
+++ PTMS/Pages/PT/Dashboard.cshtml.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PTMS.Pages.PT
 {
+    [Authorize(Roles = "PT")]
     public class DashboardModel : PageModel
     {
+        public int TotalClients { get; set; } = 0;
+        public int UpcomingSessions { get; set; } = 0;
+
         public void OnGet()
         {
+ 
         }
     }
 }
