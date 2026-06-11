0f3f6e2f22d9eed26a4d5f3e76e7ab9102fbbe62
Henry <22317272@bucks.ac.uk>
2026-06-11 18:02:39 +0100

    Fix login authentication issue

--- PTMS/Program.cs
+++ PTMS/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-    options.SignIn.RequireConfirmedAccount = true)
+    options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
