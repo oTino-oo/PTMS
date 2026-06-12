faf79c33bfa74612c403b59811e57b2eb0d3a5aa
Timothy <22320234@bucks.ac.uk>
2026-06-12 15:11:01 +0100

    Admin dashboard partial finish

--- PTMS/Pages/Admin/Dashboard.cshtml.cs
+++ PTMS/Pages/Admin/Dashboard.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PTMS.Pages.Admin
 {
+    [Authorize(Roles = "Admin")]
     public class DashboardModel : PageModel
     {
         public void OnGet()
--- PTMS/Pages/Client/Dashboard.cshtml.cs
+++ PTMS/Pages/Client/Dashboard.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PTMS.Pages.Client
 {
+    [Authorize(Roles = "Client")]
     public class DashboardModel : PageModel
     {
         public void OnGet()
